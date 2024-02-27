bb55944d25403c95ddb2efc66d04e6274381e26c
Rafael-Souza19 <rafasouza1905@outlook.com.br>
2024-02-27 17:34:42 -0300

    update
    adicionado vantagem e cores

--- dices.py
+++ dices.py
@@ -1,15 +1,44 @@
 import random
 from tkinter import *
 
+
+def alternar_desalternar():
+    if vantagem_var.get() == 1:
+        checkboxdesvantagem["state"] = "disabled"
+    else:
+        checkboxdesvantagem["state"] = "normal"
+
+    if desvantagem_var.get() == 1:
+        checkboxvantagem["state"] = "disabled"
+    else:
+        checkboxvantagem["state"] = "normal"
+
+
 def rolar_d20():
-    numero_aleatorio20 = random.randint(1, 20)
-    texto_resposta_d20["text"] = f"Resultado: {numero_aleatorio20}"
-    if numero_aleatorio20 == 20:
+    
+    if vantagem_var.get() == 1:
+        numeros_aleatorios20 = random.choices(range(1, 21), k=2)
+        texto_resposta_d20["text"] = f"Resultado: {numeros_aleatorios20[0]} / {numeros_aleatorios20[1]}"
+        if 20 in numeros_aleatorios20:
+            texto_resposta_d20["foreground"] = "green"
+            texto_resposta_d20["text"] = f"Resultado: {numeros_aleatorios20[0]} / {numeros_aleatorios20[1]}"
+        elif numeros_aleatorios20[0] == numeros_aleatorios20[1] == 1:
+            texto_resposta_d20["foreground"] = "red"
+            texto_resposta_d20["text"] = f"Resultado: {numeros_aleatorios20[0]} / {numeros_aleatorios20[1]}"
+        else:
+            texto_resposta_d20["foreground"] = "black"
+            texto_resposta_d20["text"] = f"Resultado: {numeros_aleatorios20[0]} / {numeros_aleatorios20[1]}"
+    else:
+        numero_aleatorio20 = random.randint(1, 20)
         texto_resposta_d20["text"] = f"Resultado: {numero_aleatorio20}"
-        
+        if numero_aleatorio20 == 20:
+            texto_resposta_d20["fg"] = "green"
+        elif numero_aleatorio20 == 1:
+            texto_resposta_d20["fg"] = "red"
+        else:
+            texto_resposta_d20["fg"] = "black"
 
-        
-
+             
 
 def rolar_d8():
     numero_aleatorio8 = random.randint(1, 8)
@@ -26,6 +55,7 @@ def rolar_d12():
 def rolar_d4():
     numero_aleatorio4 = random.randint(1, 4)
     texto_resposta_d4["text"] = f"Resultado: {numero_aleatorio4}"
+
 
 app = Tk()
 
@@ -77,5 +107,18 @@ botao_d4.grid(column=4, row=1, padx=10, pady=10)
 
 texto_resposta_d4 = Label(app, text="")
 texto_resposta_d4.grid(column=4, row=2, padx=10, pady=10)
+
+vantagem_var = IntVar()
+desvantagem_var = IntVar()
+
+
+checkboxvantagem=Checkbutton(app, text="Vantagem", onvalue=1,variable=vantagem_var, offvalue=0,command=alternar_desalternar)
+checkboxvantagem.grid(column=0,row=5,padx=10,pady=10)
+
+checkboxdesvantagem=Checkbutton(app, text="Desvantagem",variable=desvantagem_var, onvalue=1, offvalue=0, command=alternar_desalternar)
+checkboxdesvantagem.grid(column=4,row=5,padx=10,pady=10)
+
+
+
 
 app.mainloop()
